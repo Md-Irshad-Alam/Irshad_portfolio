93e363559e8db7f58e3125d6ecd3b7ab713887b8
MyHobbies0 <almairsh0205@gmail.com>
2022-12-16 11:57:03 +0530

    updated

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import style from "./style.css"
 export default function Contact() {
   const [name, setName] = React.useState("");
   const [email, setEmail] = React.useState("");
@@ -27,46 +27,13 @@ export default function Contact() {
   return (
     <section id="contact" className="relative">
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
-        <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-          <iframe
-            width="100%"
-            height="100%"
-            title="map"
-            className="absolute inset-0"
-            frameBorder={0}
-            marginHeight={0}
-            marginWidth={0}
-            style={{ filter: "opacity(0.7)" }}
-            src="https://www.google.com/maps/embed/v1/place?q=97+warren+st+new+york+city&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
-          />
-          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
-            <div className="lg:w-1/2 px-6">
-              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-                ADDRESS
-              </h2>
-              <p className="mt-1">
-                Dumka <br />
-                Jharkhand
-              </p>
-            </div>
-            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
-              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-                EMAIL
-              </h2>
-              <a className="text-indigo-400 leading-relaxed">
-               alamirsh0157@gmai.com
-              </a>
-              
-            </div>
-          </div>
-        </div>
         <form
           netlify
           name="test"
           onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-            Hire Me
+            Contact Us
           </h2>
           <p className="leading-relaxed mb-5">
           
@@ -113,8 +80,28 @@ export default function Contact() {
             className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
             Submit
           </button>
+          
         </form>
       </div>
+       <div className="footer">
+            <div className="cont">
+              <a href="https://github.com/Md-Irshad-Alam?tab=overview&from=2022-12-01&to=2022-12-16" className="mr-5 hover:text-white">
+              <i class="fa-brands fa-github"></i>
+            Github
+            </a>
+            
+            </div>  
+            <div className="cnt">
+              <a href="https://www.linkedin.com/in/md-irshad-alam-761b491b9/" className="mr-5 hover:text-white">
+            linkedin
+            <i class="fa-brands fa-linkedin"></i>
+            </a>
+            </div>  
+            <div className="cnt">
+            <span> <i class="fa-regular fa-envelope"></i>alamirshad0157@gmail.com </span>
+            </div>  
+        </div>
+
     </section>
   );
 }
